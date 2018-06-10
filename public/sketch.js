06d495729fd06ead45af9f7933e5d1f48ca9aa6b
david <david.journo.31@gmail.com>
2018-06-10 19:01:45 +0200

    fill function

--- public/sketch.js
+++ public/sketch.js
@@ -4,6 +4,8 @@ var sizeLine = document.getElementById("sizeLine");
 
 // reset
 var reset = document.getElementById('reset');
+// fill
+var fill = document.getElementById('fill');
 
 /*colorPicker.addEventListener("input", function () {
     var pickedColor = colorPicker.value;
@@ -46,12 +48,29 @@ function mouseDragged() {
 
 function draw() {
 
+    /* var data = {
+        x: mouseX,
+        y: mouseY,
+        px: pmouseX,
+        py: pmouseY,
+        cl: colorPicker.value,
+        size: 2*Number(sizeLine.selectedIndex+1)
+    } */
+
     // effacer le dessin /!\ ne pas enlever les commentaires au risque de tout casser
     // socket.on('resetbg', function(){
     //     background(50)
     // })
 
-    // reset.addEventListener('click', function (e){
+    // reset.addEventListener('click', function(e){
     //     socket.emit('reset', {msg:"reset"})
+    // })
+
+    // remplir le canvas avec une couleur du colorpicker
+    //socket.emit('fill', data);
+    //fill(data.cl)
+
+    //fill.addEventListener('click', function(e){
+    //     socket.emit('fill', {msg:"fill"})
     // })
 }
